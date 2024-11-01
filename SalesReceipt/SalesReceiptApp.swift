ace9196d340a69fcab0e56c285fc808d0dcc56db
kwazzar <fugitiwe@gmail.com>
2024-11-01 20:24:45 +0200

    Initial Commit

---
+++ SalesReceipt/SalesReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesReceiptApp.swift
+//  SalesReceipt
+//
+//  Created by Quasar on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SalesReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
